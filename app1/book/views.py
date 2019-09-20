b45d853ccde1049da314ec7f3c837d1bace8122e
soiqualang <soiqualang.chentreu@gmail.com>
2019-09-20 15:37:55 +0700

    add view

--- app1/book/views.py
+++ app1/book/views.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import Http404
+
 from django.shortcuts import render
+from .models import Category, Book
 
 # Create your views here.
+def index(request):
+    type_objs = Type.objects.filter(active__exact=True)
+    context = {
+        'type_objs': type_objs,
+    }
+    return render(request, "cats.html", context)
+    
+def product(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+    return render(request, 'book_detail.html', {'product': product})
