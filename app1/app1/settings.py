19792742253da57c554fee12e0864086888a83b2
soiqualang <soiqualang.chentreu@gmail.com>
2019-09-20 13:57:34 +0700

    u pass

--- app1/app1/settings.py
+++ app1/app1/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vietnam',
         'USER': 'postgres',
-        'PASSWORD': 'your-pass',
+        'PASSWORD': '2679191',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
